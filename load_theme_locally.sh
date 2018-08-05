89d9ed3bdb6d81eaf241109c1318c4f1f3b9a89a
Florian Kempenich <Flori@nKempenich.com>
2018-08-05 12:52:24 +0200

    Add script to load theme locally for dev

---
+++ load_theme_locally.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+mkdir -p ~/.antigen/bundles/FlorianKempenich/zsh-theme
+cp ./zsh-theme.zsh-theme ~/.antigen/bundles/FlorianKempenich/zsh-theme
+cp ./zsh-theme.zsh-theme ~/.antigen/bundles/FlorianKempenich/zsh-theme/zsh-theme.zsh-theme.antigen-compat
+
